89a70f7a28c08aa24ed4e6e5b0b43cd6c628434c
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2021-01-18 21:56:02 +0300

    fix Terminating cleaninig

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-kubectl get pods -n webtor | grep Terminating | awk '{print $1}' | grep seeder | xargs kubectl delete po --force --grace-period=0 -n webtor
-kubectl get pods -n webtor | grep Terminating | awk '{print $1}' | grep transcoder | xargs kubectl delete po --force --grace-period=0 -n webtor
+kubectl get pods -n webtor | grep Terminating | awk '{print $1}' | xargs kubectl delete po --force --grace-period=0 -n webtor
 kubectl get jobs --namespace="webtor" -o json | jq '.items[] | select(.status.succeeded != null or .status.failed !=null) | "kubectl delete job \(.metadata.name) -n \(.metadata.namespace)"' | xargs -n 1 sh -c
 kubectl get po --namespace="webtor" -o json | jq  '.items[] | select(.status.phase | contains("Pending")) | select(.status.conditions[0].lastTransitionTime | fromdateiso8601? < ((now | floor) - 5)) | select(.metadata.ownerReferences != null)  | .metadata.ownerReferences[0] | select(.name | match("(transcoder|seeder)")) | "kubectl delete job \(.name) -n webtor"' | xargs -n 1 sh -c
 kubectl get po --namespace="webtor" -o json | jq  '.items[] | select(.status.phase | contains("Succeeded")) | "kubectl delete po \(.metadata.name) -n \(.metadata.namespace)"' | xargs -n 1 sh -c
